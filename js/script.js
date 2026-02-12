e888828088b28f6af2a92e50a10a932c89493b79
ZhineSmathew <zhinesmoolekonil@gmail.com>
2026-02-12 16:58:27 +0400

    PWA files and directories

--- js/script.js
+++ js/script.js
@@ -244,3 +244,11 @@
     
     // Set it as the hidden input value
     document.getElementById("submissionDate").value = today;
+
+    if ("serviceWorker" in navigator) {
+    window.addEventListener("load", () => {
+        navigator.serviceWorker.register("/service-worker.js")
+        .then(reg => console.log("Service Worker registered"))
+        .catch(err => console.log("Service Worker not registered", err));
+    });
+    }
